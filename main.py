ec2d4e8db32746691fedd262eb9bea133e7aa304
SaifuddinTanzil <s.ddin.tanzil@gmail.com>
2025-08-16 21:30:18 +0600

    comment added to main py file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from routes import register_blueprints
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234@localhost/finance_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mysql123@localhost/dashcapital'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -20,4 +20,6 @@ register_blueprints(app)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=1158)
+
+#merge version 1.0 
